bf795346464453f6a00c7d1781f8e01af182389b
ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>
2023-03-04 12:34:28 +0300

    Update Notebook.h

--- Notebook.h
+++ Notebook.h
@@ -2,7 +2,7 @@
 #include <string>
 #include "Appliances.h"
 #include "Portable.h"
-class Notebook: public Portable, Appliances
+class Notebook final: public Portable, Appliances
 {
 public:
 	Notebook(int batteryLife, int weight, string color);
